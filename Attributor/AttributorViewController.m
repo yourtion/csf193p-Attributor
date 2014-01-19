0e1c23ced45874610592397604fa8b145e2bed5b
Yourtion <yourtion@gmail.com>
2014-01-19 15:56:39 +0800

    color change

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,21 +9,25 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
 
 @end
 
 @implementation AttributorViewController
 
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
