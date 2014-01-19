ccc353d02a9161fb1ea26661859df9533c99376c
Yourtion <yourtion@gmail.com>
2014-01-19 17:27:19 +0800

    Change font use NotificationCenter

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -28,6 +28,32 @@
     
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+-(void)preferredFontsChanged:(NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+-(void)usePreferredFonts
+{
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
 {
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
